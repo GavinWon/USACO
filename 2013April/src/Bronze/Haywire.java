d5e3dc9e25a5ebb26ad17cde3f6c6ae13f1287e0
Gavin <gavinmwong@gmail.com>
2019-11-29 17:17:20 -0800

    balanced teams and haywire

--- 2013April/src/Bronze/Haywire.java
+++ 2013April/src/Bronze/Haywire.java
@@ -1,16 +1,65 @@
-package Bronze;
+//package Bronze;
+
+import java.util.*;
+import java.io.*;
 
 
 public class Haywire {
 	
-	int minLength = Integer.MAX_VALUE;
-	int N;
-	int positionArray[];
+	private static int minLength = Integer.MAX_VALUE;
+	private static int N;
+	private static int positions[];
+	private static int[][] friends;
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
+		Scanner in = new Scanner(new FileReader("haywire.in"));
+		PrintWriter out = new PrintWriter(new File("haywire.out"));
+//		Scanner in = new Scanner(new FileReader("D:\\Repos\\USACO\\2013April\\src\\Bronze\\haywire.in"));
+//		PrintWriter out = new PrintWriter(new File("D:\\Repos\\USACO\\2013April\\src\\Bronze\\haywire.out"));
+		N = in.nextInt();
+		positions = new int[N + 1];
+		friends = new int[N + 1][3];
+		
+		for (int i = 1; i <= N; i++) {
+			friends[i][0] = in.nextInt();
+			friends[i][1] = in.nextInt();
+			friends[i][2] = in.nextInt();
+	}
+		
+		assign(1, 0, 0, 0);
+		
+		out.print(minLength);
+		System.out.print(minLength);
+		out.close();
+		
 		
 	}
 	
-	public static void insert (int position) {
+	public static void assign (int position, int lenSoFar, int upComingLinks, int upComingLen) {
+		if (position == N + 1) {
+			minLength = Math.min(minLength, lenSoFar);
+			return;
+		}
+		
+		for (int cow = 1; cow <= N; cow++) {	
+			if (positions[cow] == 0) {
+				positions[cow] = position;
+				int lenForThisCow = 0, existingFriends = 0, upComingFriends = 0;
+				for (int friend : friends[cow]) {
+					if (positions[friend] != 0) { //friend is already in
+						lenForThisCow += positions[cow] - positions[friend];
+						existingFriends++;
+						
+					} else { //friend is not in yet
+						upComingFriends++;
+					}
+				}
+				if (lenSoFar + upComingLen < minLength) assign(position + 1, lenSoFar + lenForThisCow, upComingLinks + upComingFriends - existingFriends,
+						upComingLen - lenForThisCow + upComingLinks);
+//				System.out.println(Arrays.toString(positions));
+				positions[cow] = 0;
+			}
+			
+		}
 	}
 }
--- 2014January/src/Bronze/BalancedTeams.java
+++ 2014January/src/Bronze/BalancedTeams.java
@@ -14,39 +14,49 @@ public class BalancedTeams {
 	public static void main(String[] args) throws IOException{
 //		Scanner in = new Scanner(new FileReader("bteams.in"));
 //		PrintWriter out = new PrintWriter(new File("bteams.out"));
-		Scanner in = new Scanner(new FileReader("/Users/gavinwong/Desktop/Repos/USACO/2014January/src/Bronze/bteams.in"));
-		PrintWriter out = new PrintWriter(new File("/Users/gavinwong/Desktop/Repos/USACO/2014January/src/Bronze/bteams.out"));
-//		lineUp[0] = 0;
-//		assigned[0] = true;
+		Scanner in = new Scanner(new FileReader("D:\\Repos\\USACO\\2014January\\src\\Bronze\\bteams.in"));
+		PrintWriter out = new PrintWriter(new File("D:\\Repos\\USACO\\2014January\\src\\Bronze\\bteams.out"));
+
 		for (int index = 0; index < 12; index++) {
 			input[index] = in.nextInt(); 
 		}
 		assign(0);
 		
 		out.print(minDifference);
+		System.out.print(minDifference);
 		out.close();
 
 	}
 	
 	public static void assign(int pos) {
+		System.out.println("Position:" + pos);
 		if (pos == 12) {
 			minDifference = Math.min(minDifference, calcSkillDifference(lineUp));
 			return;
 		}
+		int cowIDToStartAssigningToThisSlot = 0;
 		
-		for (int cowID = 0; cowID < 12; cowID++ ) {
+		if (pos % 3 == 0 && pos > 0) {
+			cowIDToStartAssigningToThisSlot = lineUp[pos - 3] + 1;
+		} else if (pos % 3 != 0 && pos > 0)
+			cowIDToStartAssigningToThisSlot = lineUp[pos - 1] + 1;
+		
+		System.out.println("CowID: " + cowIDToStartAssigningToThisSlot);
+		for (int cowID = cowIDToStartAssigningToThisSlot; cowID < 12; cowID++ ) {
 			if (!assigned[cowID]) {
-				assigned[cowID] = true;
 				lineUp[pos] = cowID;
+				System.out.println(Arrays.toString(lineUp));
+				assigned[cowID] = true;
 				assign(pos + 1);
 				assigned[cowID] = false;
 				lineUp[pos] = 0;
+				System.out.println(Arrays.toString(lineUp));
 			}
 		}
 	}
 	public static int calcSkillDifference(int[] line) {
 		int[] teamTotalSkill = new int[4];
-		for (int i = 0; i< 12; i++) {
+		for (int i = 0; i < 12; i++) {
 			teamTotalSkill[i / 3] += input[lineUp[i]];
 		}
 		int maxSkill = 0; int minSkill = Integer.MAX_VALUE;
