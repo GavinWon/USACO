70cf05c1789ebcc6b5d6fbbba5b4e8f51308b886
Gavin <gavinmwong@gmail.com>
2019-12-05 16:12:18 -0800

    diamond collector - 2016March

--- 2016March/src/Silver/DiamondCollector.java
+++ 2016March/src/Silver/DiamondCollector.java
@@ -6,70 +6,81 @@ import java.io.*;
 public class DiamondCollector {
 	
 	private static int N, K;
-	private static int[] input1, input2;
-	private static int[] diamondsNearSize;
-	private static int size1 = Integer.MIN_VALUE, size2 = Integer.MIN_VALUE;
-	private static int case1 = Integer.MIN_VALUE, case2 = Integer.MIN_VALUE;
+	private static int[] input, count, reversePreFixMax;
+	private static int maxTotal = Integer.MIN_VALUE;
 
 	public static void main(String[] args) throws IOException{
-		Scanner in = new Scanner(new FileReader("/Users/gavinwong/Desktop/Repos/USACO/2016March/src/Silver/diamond.in"));
-		PrintWriter out = new PrintWriter(new File("/Users/gavinwong/Desktop/Repos/USACO/2016March/src/Silver/diamond.out"));
+//		Scanner in = new Scanner(new FileReader("D:\\Repos\\USACO\\2016March\\src\\Silver\\diamond.in"));
+//		PrintWriter out = new PrintWriter(new File("D:\\Repos\\USACO\\2016March\\src\\Silver\\diamond.out"));
+		Scanner in = new Scanner(new FileReader("diamond.in"));
+		PrintWriter out = new PrintWriter(new File("diamond.out"));
 		
 		N = in.nextInt();
 		K = in.nextInt();
-		input1 = new int[N];
-		input2 = new int[50001];
-		diamondsNearSize = new int[50001];
+		input = new int[N];
+		count = new int[N];
+		reversePreFixMax = new int[N];
 		
 		for (int i = 0; i < N; i++) {
-			int temp = in.nextInt();
-			input1[i] = temp;
-			input2[temp] += 1;
+			input[i] = in.nextInt();
 		}
 		
-		Arrays.sort(input1);
+		Arrays.sort(input);
 		
-		
+		//Step#1 fill up count
 		for (int i = 0; i < N; i++) {
-			if (i > 0 && input1[i] == input1[i - 1]) continue;
-			int surroundings = numOfSurrDiamonds(input1[i]);
-			diamondsNearSize[input1[i]] = surroundings;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			int size = input1[i];
-			if (input2[size] + diamondsNearSize[size] > case1) {
-					if (Math.abs(size2 - size) > K) {
-						case2 = case1;
-						size2 = size1;
-					}
-					case1 = input2[size]  + diamondsNearSize[size];
-					size1 = size;
-			} else if (input2[size] + diamondsNearSize[size] > case2) {
-				if (Math.abs(size1 - size) > K) {
-					case2 = input2[size] + diamondsNearSize[size];
-					size2 = size;
+			int endPos = Arrays.binarySearch(input, input[i] + K); //endPos = the index of input[i] + K or -(insertion point) - 1
+			if (endPos < 0) { //input[i] + K doesn't exist in input
+				endPos = -(endPos + 1);
+				count[i] = endPos - i;
+			} else { //input[i] + K does exist in the input
+				while (endPos + 1 < N && input[endPos] == input[endPos + 1]) {
+					endPos++;
 				}
+				count[i] = endPos - i + 1;
 			}
 		}
-		System.out.println(size1);
-		System.out.println(size2);
-		System.out.println(case1);
-		System.out.println(case2);
-		out.println(case1 + case2);
-		out.close();
-
-	}
-	
-	public static int numOfSurrDiamonds(int size) {
-		int count = 0;
-		
-		for (int add = -K; add <= K; add++) {
-			if (size + add > 1000000000 || size + add < 1 || add == 0) continue;
-			count += input2[size + add];
+		System.out.print("Count: ");
+		for (int i = 0; i < N; i++) {
+			System.out.print(count[i] + " ");
 		}
-		System.out.println(size + " " + count);
-		return count;
+		System.out.println();
+		
+		//Step#2 fill up reversePreFixMax
+		reversePreFixMax[N - 1] = count[N - 1];
+		for (int index = N - 2; index >= 0; index--) {
+			reversePreFixMax[index] = Math.max(reversePreFixMax[index + 1], count[index]);
+		}
+		System.out.print("prefixMax: ");
+		for (int i = 0; i < N; i++) {
+			System.out.print(reversePreFixMax[i] + " ");
+		}
+		System.out.println();
+		
+		//Step#3 iterate through all possible options for Left Start + count and find the best left count and right count
+		for (int leftStart = 0; leftStart < N; leftStart++) {
+			int leftCount = count[leftStart];
+			int rightStart = leftStart + leftCount; //rightStart is the position of the starting weight for the 2nd case
+			int rightCount = 0;
+			if (rightStart < N) { //all of the diamonds are in the first case, (left case)
+				rightCount = reversePreFixMax[rightStart];
+			} else {
+				rightCount = 0;
+			}
+			if (leftCount + rightCount > maxTotal) {
+				System.out.println("LS/RS: " + leftStart + " " + rightStart);	
+				System.out.println("LC/RC: " + leftCount + " " + rightCount);
+				System.out.println();
+			}
+			maxTotal = Math.max(leftCount + rightCount, maxTotal);
+		}
+		
+		System.out.println(maxTotal);
+		out.println(maxTotal);
+		out.close();
+		
+		
+
 	}
 
 }
