905ec8648ed249b023d0598a73c23cdd673b1216
Gavin Wong <gavinmwong@gmail.com>
2019-12-09 20:56:53 -0800

    2018Dec -Mooyo (unfinished2)

--- 2018December/src/Silver/MooyoMooyo.java
+++ 2018December/src/Silver/MooyoMooyo.java
@@ -1,4 +1,4 @@
-package Silver;
+//package Silver;
 
 import java.util.*;
 import java.io.*;
@@ -10,8 +10,10 @@ public class MooyoMooyo {
 	private static boolean gridChanged;
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new FileReader("D:\\Repos\\USACO\\2018December\\src\\Silver\\mooyomooyo.in"));
-		PrintWriter out = new PrintWriter(new File("D:\\Repos\\USACO\\2018December\\src\\Silver\\mooyomooyo.out"));
+		BufferedReader in = new BufferedReader(new FileReader("mooyomooyo.in"));
+		PrintWriter out = new PrintWriter(new File("mooyomooyo.out"));
+//		BufferedReader in = new BufferedReader(new FileReader("/Users/gavinwong/Desktop/Repos/USACO/2018December/src/Silver/mooyomooyo.in"));
+//		PrintWriter out = new PrintWriter(new File("/Users/gavinwong/Desktop/Repos/USACO/2018December/src/Silver/mooyomooyo.out"));
 		
 		StringTokenizer firstLine = new StringTokenizer(in.readLine());
 		N = Integer.parseInt(firstLine.nextToken());
@@ -26,52 +28,65 @@ public class MooyoMooyo {
 			}
 		}
 		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < 10; j++) {
-				System.out.print(grid[i][j]);
-			}
-			System.out.println();
-		}
+//		printGrid();
+		
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < 10; j++) {
+//				System.out.print(grid[i][j]);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
 		
 		do{
 			gridChanged = false;
+			reformGrid();
 			for (int row = N - 1; row >= 0; row--) {
 				for (int col = 0; col < 10; col++) {
 					if (!(grid[row][col] == 0)) { 
-						boolean delete = floodfillcheck(row, col, grid[row][col], 0);
-						System.out.println("Color: " + grid[row][col] + " Row/Col: " + row + "/" + col);
+						boolean[][] visited = new boolean[N][10];
+						boolean delete = floodfillcheck(row, col, grid[row][col], 1, visited);
+//						System.out.println("Color: " + grid[row][col] + " Row/Col: " + row + "/" + col);
 						if (delete) floodfilldelete(row, col, grid[row][col]);
+//						printGrid();
 					}
 				}
 			}
-			reformGrid();
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < 10; j++) {
-					System.out.print(grid[i][j]);
-				}
-				System.out.println();
-			}
-			System.out.println("-------------------------");
 		} while (gridChanged);
 		
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 10; j++) {
+//				System.out.print(grid[i][j]);
+				out.print(grid[i][j]);
+			}
+			out.println();
+//			System.out.println();
+		}
+		out.close();
+
+	}
+	
+	public static void printGrid (){
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < 10; j++) {
 				System.out.print(grid[i][j]);
 			}
 			System.out.println();
 		}
-		
-		out.close();
-
+		System.out.println("-------------------------");
 	}
-	public static boolean floodfillcheck(int row, int  col, int color, int count) {
+	public static boolean floodfillcheck(int row, int  col, int color, int count, boolean[][] visited) {
 		if (row >= N || row < 0 || col >= 10 || col < 0) return false;
 		if (grid[row][col] != color) return false;
+		if (visited[row][col]) return false;
+		visited[row][col] = true;
 		if (count == K) return true;
-		return floodfillcheck(row + 1, col, color, count + 1) ||
-		floodfillcheck(row - 1, col, color, count + 1) ||
-		floodfillcheck(row, col + 1, color, count + 1) ||
-		floodfillcheck(row, col - 1, color, count + 1);
+		
+		return floodfillcheck(row + 1, col, color, count + 1, visited) ||
+		floodfillcheck(row - 1, col, color, count + 1, visited) ||
+		floodfillcheck(row, col + 1, color, count + 1,visited) ||
+		floodfillcheck(row, col - 1, color, count + 1, visited);
 		
 	}
 	
