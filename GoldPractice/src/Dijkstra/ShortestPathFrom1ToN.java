7fbb69ce14e9bd1056f51118abbaf79226c59d78
Gavin <gavinmwong@gmail.com>
2019-12-25 18:09:53 -0800

    finished gfg 1toN

--- GoldPractice/src/Dijkstra/ShortestPathFrom1ToN.java
+++ GoldPractice/src/Dijkstra/ShortestPathFrom1ToN.java
@@ -3,49 +3,46 @@ package Dijkstra;
 import java.util.*;
 import java.io.*;
 
-public class ShortestPathFrom1ToN {
-	
-	public static class Node implements Comparable<Node>{
-		int distance = Integer.MAX_VALUE;
-		int value = 0;
-		public Node (int val, int distance) {
-			value = val;
-			this.distance = distance;
-		}
-		
-		public int compareTo(Node n) {
-			return distance - n.distance;
-		}
-	}
+public class ShortestPathFrom1ToN { 
 	
 	private static int[] distances;
-	private static boolean[] visited;
+	private static boolean[] addedToQueue; //similar to visited
 
 	public static void main(String[] args) throws IOException{
-		int N = 9;
-		distances = new int[N + 1];
-		visited = new boolean[N + 1];
-		PriorityQueue<Node> q = new PriorityQueue<Node>();
-		q.add(new Node(2, 1));
-		q.add(new Node(3, 1));
-		visited[1] = true;
 		
-		while(!q.isEmpty() || q.peek().value != N) {
-			Node currentNode = q.remove();
-			int val = currentNode.value;
-			int distance = currentNode.distance;
-			distances[val] = distance;
-			visited[val] = true;
-			if (val * 3 <= N && (distances[val * 3] == 0 || distances[val * 3] > distances[val] + 1)) {
-				distances[val * 3] = distances[val] + 1;
-				q.add(new Node(val * 3, distances[val * 3]));
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for (int i = 0; i < T; i++) {
+			int N = in.nextInt();
+			distances = new int[N + 1];
+			Arrays.fill(distances, Integer.MAX_VALUE);
+			distances[1] = 0;
+			addedToQueue = new boolean[N + 1];
+			PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+			q.add(1);
+			addedToQueue[1] = true;
+			
+			while(!q.isEmpty() && q.peek() != N) {
+				int currentNode = q.remove();
+				int add1 = currentNode + 1;
+				int times3 = currentNode * 3;
+				if (add1 <= N) {
+					distances[add1] = Math.min(distances[add1], distances[currentNode] + 1);
+					if (!addedToQueue[add1]) {
+						q.add(add1);
+						addedToQueue[add1] = true;
+					}
+				}
+				if (times3 <= N) {
+					distances[times3] = Math.min(distances[times3], distances[currentNode] + 1);
+					if (!addedToQueue[times3]) {
+						q.add(times3);
+						addedToQueue[times3] = true;
+					}
+				}
+				
 			}
-			if (val + 1 <= N && (distances[val + 1] == 0 || distances[val + 1] > distances[val] + 1)) {
-				distances[val * 3] = distances[val] + 1;
-				q.add(new Node(val + 1, distances[val + 1]));
-			}
+			System.out.println(distances[N]);
 		}
-		System.out.println(distances[N]);
 	}
-
 }
